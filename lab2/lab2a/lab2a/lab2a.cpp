445ad9948390de4f8a715448e81112ea85d2fc25
mrcm777@gmail.com <mrcm777@gmail.com>
2022-10-21 01:16:24 +0200

    Initial commit

---
+++ lab2/lab2a/lab2a/lab2a.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <limits>
+
+class TQuadEq
+{
+private:
+    double _a;
+    double _b;
+    double _c;
+
+public:
+    TQuadEq() : _a(0.0), _b(0.0), _c(0.0) {};
+    TQuadEq(double a, double b, double c) : _a(a), _b(b), _c(c) {};
+
+
+    double GetA() const { return _a; }
+    double GetB() const { return _b; }
+    double GetC() const { return _c; }
+
+    void SetA(const double value) { _a = value; }
+    void SetB(const double value) { _b = value; }
+    void SetC(const double value) { _c = value; }
+
+    double ComputeDelta() const
+    {
+        return _b * _b - 4 * _a * _c;
+    }
+
+    int GetNumRoots(const double delta)
+    {
+        if (delta < 0) { return 0; }
+        else if (delta > 0) { return 2; }
+        else { return 1; }
+    }
+
+    void GetRoots(double& root1, double& root2) const
+    {
+        root1 = (-_b + sqrt(ComputeDelta())) / (2 * _a);
+        root2 = (-_b - sqrt(ComputeDelta())) / (2 * _a);
+    }
+
+};
+
+
+int main()
+{
+    double a = std::numeric_limits<double>::signaling_NaN();
+    double b = std::numeric_limits<double>::signaling_NaN();
+    double c = std::numeric_limits<double>::signaling_NaN();
+    double root1 = std::numeric_limits<double>::signaling_NaN();
+    double root2 = std::numeric_limits<double>::signaling_NaN();
+
+    std::cout << "Enter coefficients for a+b^2+c=0 quadratic equation" << std::endl;
+    std::cout << "a:\t";
+    std::cin >> a;
+    std::cout << "b:\t";
+    std::cin >> b;
+    std::cout << "c:\t";
+    std::cin >> c;
+    
+    TQuadEq sq1(a, b, c);
+    int delta = sq1.ComputeDelta();
+    int rootNum = sq1.GetNumRoots(delta);
+    sq1.GetRoots(root1, root2);
+
+
+    std::cout << "RESULTS:\n" << std::endl;
+    std::cout << "delta = " << sq1.ComputeDelta() << std::endl;
+    std::cout << "number of real roots = " << rootNum << std::endl;
+    std::cout << "root1 = " << root1 << std::endl;
+    std::cout << "root2 = " << root2 << std::endl;
+    return 0;
+
+}  
---
+++ lab2/lab2b/lab2b/lab2b.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+class MyString : public std::string
+{
+private:
+    const char *_str;
+
+public:
+    MyString(const char* str = "MyString")
+    {
+        std::cout << "MyString constructor called" << std::endl;
+        _str = str;
+    }
+    ~MyString()
+    {
+        std::cout << "MyString destructor called" << std::endl;
+    }
+
+    const char* GetValue() const { return _str; }
+};
+
+std::ostream& operator<<(std::ostream& out, const MyString& rhs)
+{
+    out << "MyString:" << rhs.GetValue() << std::endl;
+    return out;
+}
+
+int main()
+{
+    MyString str1 = "Hello World!";
+    MyString str2("Important Info");
+
+    std::cout << str1 << std::endl;
+    std::cout << str2 << std::endl;
+
+    
+    return 0;
+}
